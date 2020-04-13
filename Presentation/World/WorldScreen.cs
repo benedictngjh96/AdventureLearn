fc573d1ce41d47abd2860cab95a7169c57729f42
ben132473u <55075779+ben132473u@users.noreply.github.com>
2020-04-13 16:35:41 +0800

    Fixed unable to build error

--- Presentation/World/WorldScreen.cs
+++ Presentation/World/WorldScreen.cs
@@ -5,63 +5,55 @@ using System.Linq;
 
 public class WorldScreen : Node
 {
-<<<<<<< HEAD
-	TextureButton world1Btn, world2Btn, world3Btn;
-=======
-	TextureButton world1Btn, world2Btn, world3Btn; 
->>>>>>> d2310c65f66c956cba5c8b9583ad14022d5d76f9
-	WorldBL worldBl;
+    TextureButton world1Btn, world2Btn, world3Btn;
+    WorldBL worldBl;
 
-	public override void _Ready()
-	{
-		//Testing
-<<<<<<< HEAD
-		//Global.StudentId = 27;
-=======
-		//Global.StudentId = 29;
->>>>>>> d2310c65f66c956cba5c8b9583ad14022d5d76f9
-		//Testing
+    public override void _Ready()
+    {
+        //Testing
+        //Global.StudentId = 27;
+        //Testing
 
-		world1Btn = GetNode<TextureButton>("Bg/World1");
-		world2Btn = GetNode<TextureButton>("Bg/World2");
-		world3Btn = GetNode<TextureButton>("Bg/World3");
+        world1Btn = GetNode<TextureButton>("Bg/World1");
+        world2Btn = GetNode<TextureButton>("Bg/World2");
+        world3Btn = GetNode<TextureButton>("Bg/World3");
 
-		worldBl = new WorldBL();
+        worldBl = new WorldBL();
 
-		disableInaccessibleWorlds();
-	}
+        disableInaccessibleWorlds();
+    }
 
-	private void disableInaccessibleWorlds()
-	{
+    private void disableInaccessibleWorlds()
+    {
 
-		int completedWorldCount = worldBl.getCompletedWorldCount();
-		int totalWorldCount = worldBl.getTotalWorldCount();
+        int completedWorldCount = worldBl.getCompletedWorldCount();
+        int totalWorldCount = worldBl.getTotalWorldCount();
 
 
-		for (int i = totalWorldCount; i > completedWorldCount + 1 ; i--)
-		{
-			TextureButton disabler = GetNode<TextureButton>("Bg/World" + i);
-			disabler.Disabled = true;
-		}
-	}
+        for (int i = totalWorldCount; i > completedWorldCount + 1 ; i--)
+        {
+            TextureButton disabler = GetNode<TextureButton>("Bg/World" + i);
+            disabler.Disabled = true;
+        }
+    }
 
-	private void _on_World1_pressed()
-	{
-		Global.WorldId = 1;
-		GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
-	}
+    private void _on_World1_pressed()
+    {
+        Global.WorldId = 1;
+        GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
+    }
 
-	private void _on_World2_pressed()
-	{
-		Global.WorldId = 2;
-		GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
-	}
+    private void _on_World2_pressed()
+    {
+        Global.WorldId = 2;
+        GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
+    }
 
-	private void _on_World3_pressed()
-	{
-		Global.WorldId = 3;
-		GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
-	}
+    private void _on_World3_pressed()
+    {
+        Global.WorldId = 3;
+        GetTree().ChangeScene("res://Presentation/LevelSelection/LevelSelection.tscn");
+    }
 }
 
 
